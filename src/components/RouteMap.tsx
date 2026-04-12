223f7a9f5c1c3c3c5849f013d0ce3a126a47cfe1
Ata Can <atacanymc@gmail.com>
2026-04-13 02:00:38 +0300

    feat: update map tile layer attribution and URL to use CARTO service

--- src/components/RouteMap.tsx
+++ src/components/RouteMap.tsx
@@ -76,8 +76,8 @@ export function RouteMap({ guzergah, className = "" }: RouteMapProps) {
       style={{ minHeight: "400px" }}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
       />
 
       {/* Gidiş rotası - Yeşil */}
